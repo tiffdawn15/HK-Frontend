1a87c2fa27c737b9262aa9f023f907817493c61b
tiffanymesser <tiffanymesser15@gmail.com>
2019-10-24 15:50:34 -0400

    Ran NPM build

---
+++ build/precache-manifest.39ce6c6713c033dbf9b5ff5bf50fb20a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a45059318d8b2efe30ae58ed3205128",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6dba67918904859f1cba",
+    "url": "/static/css/main.1b398baf.chunk.css"
+  },
+  {
+    "revision": "37cdecbb79d6ebdc8f71",
+    "url": "/static/js/2.483640e4.chunk.js"
+  },
+  {
+    "revision": "6dba67918904859f1cba",
+    "url": "/static/js/main.4cf8a8c2.chunk.js"
+  },
+  {
+    "revision": "c58abb68cf566dc89980",
+    "url": "/static/js/runtime-main.7d7f9635.js"
+  }
+]);
